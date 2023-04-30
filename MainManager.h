c838bf1fd8a0449be2bfe52e2a0634a51dc2d8de
MohamedGamalBarghash <cinosmoh@gmail.com>
2023-04-30 14:53:11 +0300

    reverter

--- MainManager.h
+++ MainManager.h
@@ -18,29 +18,27 @@ public:
     char gender;
     string vaccineType;
     bool firstDose;
-
-
     string firstDoseDate;
     bool secondDose;
     string secondDoseDate;
-
-
-
-
 };
 
 class MainManager {
 private:
-    unordered_map<string, Entry> entries; // Map to store the entries
+    unordered_map<int, Entry> entries; // Map to store the entries
     queue<Entry> waitingList; // Queue to store the entries waiting for vaccination
 
 public:
     // Function to create a new entry and add it to the entries vector or waiting list
-    void CreateEntry(string name, int age, string vaccineType, bool vaccinated);
+    void CreateEntry(string name, int id, string government, int age, char gender, string vaccineType, bool firstDose, string firstDate, bool secondDose, string secondDate);
+    // Function to edit an entry and add it to the waiting list if not fully vaccinated
+    bool EditEntry(int id, Entry newEntry);
     // Function to check the vaccination status of a given entry
-    bool CheckVaccineStatus(string name);
+    bool CheckVaccineStatus(int id);
     // Function to delete an entry from the entries vector or waiting list
-    void DeleteEntry(string name);
+    void DeleteEntry(int id);
+    // Function to show a certain entry
+    void ShowEntry(int id);
     // Function to show all entries and waiting list
     void ShowAll();
     // Function to write the entries and waiting list to a CSV file
