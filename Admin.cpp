632b52b0d49160474550c410c60d177beb3cd661
AbdoTarek2211 <abdo_bemo@hotmail.com>
2023-04-30 23:09:10 +0300

    lets hope it works

--- Admin.cpp
+++ Admin.cpp
@@ -61,5 +61,70 @@ void Admin::ViewDoses()
 
 float Admin::ViewStatistics()
 {
-	return 0.0f;
+    int temp = 0;
+    int males = 0;
+    int females = 0;
+    char input;
+    cout << "Type F To see the The Amount of People Who Recieved The first Dose only Or Type B to see the Amount of People Who Recieved Both Doses Or Type G To See the Amount of Males and Females on The System" << endl;
+    cin >> input;
+    if (input == 'F' || input == 'f')
+    {
+        
+        queue<Entry> tempList = waitingList;
+        while (!tempList.empty()) {
+            if (tempList.front().firstDose == true)
+            {
+                temp++;
+            }
+            tempList.pop();
+        }
+        tempList.~queue();
+        return (temp / entries.size());
+    }
+    else if (input == 'B' || input == 'b')
+    {
+        for (pair<int, Entry> entry : entries) {
+            if (entry.second.firstDose == true && entry.second.secondDose == true)
+            {
+                temp++;
+            }
+        }
+        return (temp / entries.size());
+    }
+    else if (input == 'G' || input == 'g')
+    {
+        for (pair<int, Entry> entry : entries) {
+            if (entry.second.age == 'M' || entry.second.age == 'm')
+            {
+                males++;
+            }
+        }
+        queue<Entry> tempList = waitingList;
+        while (!tempList.empty()) {
+            if (tempList.front().age == 'M' || tempList.front().age == 'm')
+            {
+                males++;
+            }
+            tempList.pop();
+        }
+        tempList.~queue();
+        return (males / entries.size());
+        for (pair<int, Entry> entry : entries) {
+            if (entry.second.age == 'F' || entry.second.age == 'f')
+            {
+                females++;
+            }
+        }
+        queue<Entry> tempList = waitingList;
+        while (!tempList.empty()) {
+            if (tempList.front().age == 'F' || tempList.front().age == 'f')
+            {
+                females++;
+            }
+            tempList.pop();
+        }
+        tempList.~queue();
+        return (females / entries.size());
+    }
+
 }
